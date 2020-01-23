4f550e60dbd3e959e1513df897d5e8f949244930
Brandon-Harris <brandonharris177@gmail.com>
2020-01-22 18:09:33 -0700

    passing props have to finish setting up app

--- src/App.js
+++ src/App.js
@@ -4,15 +4,21 @@ PostsContainer components and the CommentSection Components.
 Not all files need code added. 
 Look at each file to see where you need to pass props or add code 
 */
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 // import the PostsPage and SearchBar and add them to the App
 // import dummydata
+import SearchBar from "./components/SearchBar/SearchBarContainer"
+import PostsPage from "./components/PostsContainer/PostsPage";
+import dummydata from "./dummy-data";
 
 const App = () => {
+  const [dummydata] = useState(dummydata)
   // set up state for the dummy data and pass to your PostsPage
   return (
     <div className="App">
+      <SearchBar/>
+      <PostsPage data = {"dummydata"}/>
       {/* Add components here  and pass props where appropriate */}
     </div>
   );
--- src/components/CommentSection/CommentSectionContainer.js
+++ src/components/CommentSection/CommentSectionContainer.js
@@ -6,13 +6,19 @@ import "./Comment.css";
 
 const CommentSection = props => {
   // Add state for the comments
+  const [comments] = useState(props.comments)
 
   return (
     <div>
-      {/* map through the comments data and return the Comment component */}
+      {console.log(props)}
+      {comments.map((comment) => <Comment
+        key = {comment.index} comment = {comment.comment} />
+      )}
       <CommentInput />
     </div>
   );
 };
 
 export default CommentSection;
+
+/* map through the comments data and return the Comment component */
